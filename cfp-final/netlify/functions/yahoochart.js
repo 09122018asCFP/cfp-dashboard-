b5583af9ad2704b7d21e862cd2b5d2a626fdaaa6
09122018asCFP <aascordia@gmail.com>
2026-03-23 18:39:37 -0300

    Update yahoochart.js

--- cfp-final/netlify/functions/yahoochart.js
+++ cfp-final/netlify/functions/yahoochart.js
@@ -2,17 +2,16 @@ exports.handler = async (event) => {
   const sym = event.queryStringParameters?.sym || 'SPY';
   const period1 = event.queryStringParameters?.period1 || '1735516800';
   const period2 = event.queryStringParameters?.period2 || '1735689600';
-  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${sym}?interval=1d&period1=${period1}&period2=${period2}`;
-
-  const headers = {
-    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-    'Accept': '*/*',
-    'Referer': 'https://finance.yahoo.com/',
-    'Origin': 'https://finance.yahoo.com',
-  };
 
   try {
-    const res = await fetch(url, { headers });
+    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${encodeURIComponent(sym)}?interval=1d&period1=${period1}&period2=${period2}`;
+    const res = await fetch(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
+        'Accept': 'application/json',
+        'Referer': 'https://finance.yahoo.com',
+      }
+    });
     const data = await res.json();
     return {
       statusCode: 200,
