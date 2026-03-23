4cda2736ccb1548adef7d1d189034dac1d4b7a5d
09122018asCFP <aascordia@gmail.com>
2026-03-23 17:47:52 -0300

    Add files via upload

---
+++ cfp-final/netlify/functions/dolar.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  try {
+    const res = await fetch('https://dolarapi.com/v1/dolares');
+    const data = await res.json();
+    return { statusCode: 200, headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify(data) };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+  }
+};
