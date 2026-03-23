f6b5f30f66adc389449e5c32f6ee1df2324a3517
09122018asCFP <aascordia@gmail.com>
2026-03-23 17:47:23 -0300

    Delete cfp-dashboard directory

--- cfp-dashboard/netlify/functions/bcra.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-  try {
-    const res = await fetch('https://api.bcra.gob.ar/estadisticas/v2.0/principalesvariables');
-    const data = await res.json();
-    return { statusCode: 200, headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify(data) };
-  } catch (e) {
-    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
-  }
-};
